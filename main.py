0cd9562612a312ddb969fefbafd83bd333071ef0
Thein Lin Aung <theinlinaung2010@gmail.com>
2025-09-21 17:22:18 +0900

    Anthropic MCP weather server and client

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from src.weather_mockup_nomcp import WeatherMonitorMockup
+from src.mcp_client_anthropic import MCPClient
+import asyncio
+import sys
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 
 
-def main():
-    scheduler = WeatherMonitorMockup()
-    scheduler.run_monitoring()
+async def client_loop(server_script_path: str):
+    client = MCPClient()
+    try:
+        await client.connect_to_server(server_script_path)
+        await client.chat_loop()
+    finally:
+        await client.cleanup()
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 2:
+        print("Usage: python client.py <path_to_server_script>")
+        sys.exit(1)
+    asyncio.run(client_loop(sys.argv[1]))
